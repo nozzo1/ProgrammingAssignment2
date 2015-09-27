fbdee6089a7a142584fd99d49af3d3404ee20f90
nozzo1 <nozzo1@gmail.com>
2015-09-27 22:05:52 +0100

    final code commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function processes the input matrix parameters and outputs to x
+## it also contains two further functions below.
 
 makeCacheMatrix <- function(x = matrix()) {
   iMat <- NULL
   set <- function(matY) {
+    # note the use of the double arrow to save data to the parent environment.
     x <<- matY
     iMat <<- NULL
   }
   get <- function() x
+  # setup the Set Matrix Inverse function
   setMatInv <- function(inverse) iMat <<- inverse
+  # setup the Get Matrix Inverse function
   getMatInv <- function() iMat
+  # now initialise everything above.
   list(set = set, get = get,
        getMatInv = getMatInv,
        setMatInv = setMatInv)
 }
 
 
-## Write a short comment describing this function
+## here we use the various functions previously set to produce
+## an inverse matrix and store the output in to our cache 'iMat'.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x' and save to iMat
 iMat <- x$getMatInv()
+  ## now test iMat for cached data or if it is null.
   if (!is.null(iMat)) {
       message("getting cached data")
       return(iMat)
   }
+  ## output matrix to fMat
   fMat <- x$get()
+  ## solve matrix and store in iMat
   iMat <- solve(fMat, ...)
+  ## use the inverse function to store inverse of matrix iMat
   x$setMatInv(iMat)
+  ## output iMat
   iMat
   }
